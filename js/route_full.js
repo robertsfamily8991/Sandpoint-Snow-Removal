d24a7b8b0cbc4d6b83aeb3b8331420a1bdbb571b
robertsfamily8991 <103419028+robertsfamily8991@users.noreply.github.com>
2022-12-11 15:15:23 -0800

    Update route_full.js

--- js/route_full.js
+++ js/route_full.js
@@ -1,1 +1,1 @@
-alert("\n\n\nOUR ROUTES ARE FULL FOR THE SEASON\n\n__________________________________\n\nWE RECOMMENDED:\n\n• YARD YETI: (208) 597-2307\n\n\n\n\n\nCall us to be added to our recommendations!");
+alert("SANDPOINT SNOW REMOVAL LLC\n\nOUR ROUTES ARE FULL FOR THE SEASON\n\n_______________________________________\n\nWE RECOMMENDED:\n\n• YARD YETI: (208) 597-2307\n\n\n\n\n\nCall us to be added to our recommendations!");
