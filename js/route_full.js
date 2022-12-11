aa0198360ca135cdf45c6e307b904f3835c1db1e
robertsfamily8991 <103419028+robertsfamily8991@users.noreply.github.com>
2022-12-11 14:56:44 -0800

    Add JS Files

---
+++ js/route_full.js
@@ -0,0 +1,1 @@
+alert("SANDPOINT SNOW REMOVAL LLC\n\nOUR ROUTES ARE FULL FOR THE SEASON\n\n_______________________________________\n\nWE RECOMMENDED:\n\n• YARD YETI: (208) 597-2307\n\n\n\n\n\nCall us to be added to our recommendations!");
