c94200016ac282b9de44e6a74e48633418e3b9a8
robertsfamily8991 <103419028+robertsfamily8991@users.noreply.github.com>
2024-12-30 17:52:02 -0800

    Update Google Sheet.js

--- Google Sheet.js
+++ Google Sheet.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', e => {
   e.preventDefault()
   
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! Form is submitted/n/nWe will contact you very soon with more information." ))
+  .then(response => alert("Thank you! Form is submitted./n/nWe will contact you very soon with more information." ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
